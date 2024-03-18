93e34d4cb4ae3d72549015170ae0f3d551a00a65
opilane <opilane@HP-CZC2349HTZ>
2024-03-18 13:20:36 +0200

    aa

---
+++ Maks_telef/Maks_telef/Ruhm.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Maks_telef
+{
+    internal class Ruhm
+    {
+    }
+}
---
+++ Maks_telef/Maks_telef/Telefon.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Maks_telef
+{
+    public class Telefon
+    {
+        public string Nimetus { get; set; }
+        public string Tootja { get; set; }
+        public int Hind { get; set; }
+        public string Pilt { get; set; }
+
+        public Telefon(string nimetus, string tootja, int hind, string pilt)
+        {
+            Nimetus = nimetus;
+            Tootja = tootja;
+            Hind = hind;
+            Pilt = pilt;
+            
+        }
+    }
+}
